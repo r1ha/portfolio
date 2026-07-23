b836b5d9c5f00bbefff010ba9422779b0359afaa
erwan.achat <erwan.achat@universite-paris-saclay.fr>
2026-07-24 01:45:25 +0200

    feat: github link inside footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NAV_SECTIONS } from '../../info/section-info';
+import { NgOptimizedImage } from '@angular/common';
+import { PERSONAL_INFO } from '../../info/personal-info';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   protected readonly navSections = inject(NAV_SECTIONS);
+  protected readonly personalInfo = inject(PERSONAL_INFO);
+  
 }
